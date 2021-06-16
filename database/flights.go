62719870eeb7586e2b189232e4d7e91562370e41
Daniel Hawton <daniel@hawton.org>
2021-06-15 20:21:42 -0500

    created at and updated at columns

--- database/flights.go
+++ database/flights.go
@@ -1,13 +1,18 @@
 package database
 
+import "time"
+
 type Flights struct {
-	Callsign    string  `json:"callsign" gorm:"primaryKey;type:varchar(10)"`
-	CID         int     `json:"cid" gorm:"index"`
-	Latitude    float32 `json:"latitude" gorm:"type:float(10,8)"`
-	Longitude   float32 `json:"longitude" gorm:"type:float(11,8)"`
-	Groundspeed int     `json:"groundspeed"`
-	Heading     int     `json:"heading"`
-	Altitude    int     `json:"altitude"`
-	Aircraft    string  `json:"aircraft" gorm:"type:varchar(6)"`
-	Route       string  `json:"route" gorm:"type:text"`
+	ID          int       `json:"id" gorm:"primaryKey"`
+	Callsign    string    `json:"callsign" gorm:"index;type:varchar(10)"`
+	CID         int       `json:"cid" gorm:"index"`
+	Latitude    float32   `json:"latitude" gorm:"type:float(10,8)"`
+	Longitude   float32   `json:"longitude" gorm:"type:float(11,8)"`
+	Groundspeed int       `json:"groundspeed"`
+	Heading     int       `json:"heading"`
+	Altitude    int       `json:"altitude"`
+	Aircraft    string    `json:"aircraft" gorm:"type:varchar(6)"`
+	Route       string    `json:"route" gorm:"type:text"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
--- database/user.go
+++ database/user.go
@@ -3,14 +3,14 @@ package database
 import "time"
 
 type User struct {
-	CID            uint   `json:"cid" gorm:"primaryKey"`
-	FirstName      string `json:"firstname" gorm:"type:varchar(128)"`
-	LastName       string `json:"lastname" gorm:"type:varchar(128)"`
-	Email          string `json:"-" gorm:"type:varchar(128)"`
-	ControllerType string `json:"controllerType" gorm:"type:enum('none' 'visitor' 'home')"`
-	RatingID       int    `json:"-"`
-	Rating         Rating `json:"rating"`
-	Status         string `json:"status" gorm:"type:enum('none' 'active' 'inactive' 'leave')"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	CID            uint      `json:"cid" gorm:"primaryKey"`
+	FirstName      string    `json:"firstname" gorm:"type:varchar(128)"`
+	LastName       string    `json:"lastname" gorm:"type:varchar(128)"`
+	Email          string    `json:"-" gorm:"type:varchar(128)"`
+	ControllerType string    `json:"controllerType" gorm:"type:enum('none' 'visitor' 'home')"`
+	RatingID       int       `json:"-"`
+	Rating         Rating    `json:"rating"`
+	Status         string    `json:"status" gorm:"type:enum('none' 'active' 'inactive' 'leave')"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
