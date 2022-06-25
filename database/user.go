c5a02eea21d1e30a0c6c1c8f69d4fa752b21ac28
Daniel Hawton <daniel@hawton.org>
2022-06-24 23:51:56 -0600

    remove enums

--- database/user.go
+++ database/user.go
@@ -1,48 +1,27 @@
 package database
 
 import (
-	"database/sql/driver"
 	"time"
 )
 
 type User struct {
-	CID               uint        `json:"cid" gorm:"primaryKey"`
-	FirstName         string      `json:"firstname" gorm:"type:varchar(128)"`
-	LastName          string      `json:"lastname" gorm:"type:varchar(128)"`
-	Email             string      `json:"email" gorm:"type:varchar(128)"`
-	OperatingInitials string      `json:"oi" gorm:"type:varchar(2)"`
-	ControllerType    string      `json:"controllerType" gorm:"type:varchar(10)"`
-	DelCertification  CertOptions `json:"delCertification" gorm:"type:varchar(15)"`
-	GndCertification  CertOptions `json:"gndCertification" gorm:"type:varchar(15)"`
-	LclCertification  CertOptions `json:"lclCertification" gorm:"type:varchar(15)"`
-	AppCertification  CertOptions `json:"appCertification" gorm:"type:varchar(15)"`
-	CtrCertification  CertOptions `json:"ctrCertification" gorm:"type:varchar(15)"`
-	RatingID          int         `json:"-"`
-	Rating            Rating      `json:"rating"`
-	Status            string      `json:"status" gorm:"type:varchar(10)"`
-	Roles             []*Role     `json:"roles" gorm:"many2many:user_roles"`
-	CreatedAt         time.Time   `json:"created_at"`
-	UpdatedAt         time.Time   `json:"updated_at"`
-}
-
-type CertOptions string
-
-const (
-	NONE      CertOptions = "none"
-	TRAINING  CertOptions = "training"
-	SOLO      CertOptions = "solo"
-	CERTIFIED CertOptions = "certified"
-	MAJOR     CertOptions = "major"
-	CAN_TRAIN CertOptions = "cantrain"
-)
-
-func (co *CertOptions) Scan(value interface{}) error {
-	*co = CertOptions(value.([]uint8))
-	return nil
-}
-
-func (co CertOptions) Value() (driver.Value, error) {
-	return string(co), nil
+	CID               uint      `json:"cid" gorm:"primaryKey"`
+	FirstName         string    `json:"firstname" gorm:"type:varchar(128)"`
+	LastName          string    `json:"lastname" gorm:"type:varchar(128)"`
+	Email             string    `json:"email" gorm:"type:varchar(128)"`
+	OperatingInitials string    `json:"oi" gorm:"type:varchar(2)"`
+	ControllerType    string    `json:"controllerType" gorm:"type:varchar(10)"`
+	DelCertification  string    `json:"delCertification" gorm:"type:varchar(15)"`
+	GndCertification  string    `json:"gndCertification" gorm:"type:varchar(15)"`
+	LclCertification  string    `json:"lclCertification" gorm:"type:varchar(15)"`
+	AppCertification  string    `json:"appCertification" gorm:"type:varchar(15)"`
+	CtrCertification  string    `json:"ctrCertification" gorm:"type:varchar(15)"`
+	RatingID          int       `json:"-"`
+	Rating            Rating    `json:"rating"`
+	Status            string    `json:"status" gorm:"type:varchar(10)"`
+	Roles             []*Role   `json:"roles" gorm:"many2many:user_roles"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
 }
 
 var CertificationOptions = map[string]string{
